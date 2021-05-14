fed3bb9d0aeb60105127dc5d889608afb052d7df
kyboahene <kyeiyaw437@gmail.com>
2021-05-14 00:30:40 -0800

    replaced placeholders

--- src/components/team.js
+++ src/components/team.js
@@ -7,65 +7,54 @@ const Team = () => {
     <section className="container-fluid team">
       <div className="container">
         <div className="row d-flex justify-content-center pt-5">
-          <div className="col text-center u-font-raleway">
-            <h1>Our Team</h1>
-            <p className=" u-custom-font u-font-raleway">
-              Sample text. Click to select the text box. Click again or double
-              click to start editing the text.
+          <div className="col text-center u-custom-font u-font-raleway">
+            <h1 className="u-custom-font u-font-raleway font-weight-bold">
+              Our Team
+            </h1>
+            <p className=" u-custom-font u-font-raleway lead">
+              The brains behind the great firm.
             </p>
           </div>
         </div>
         <div className="row text-center py-5">
-          <div className="col-md-3">
+          <div className="col-md-4">
             <img
               src={Image}
               className="img-fluid rounded-circle"
               alt=""
               data-aos="fade-up"
             />
-            <h4 className="mb-0">Joe Dadzie</h4>
-            <p className="my-0">CEO</p>
+            <h4 className="mb-0 u-custom-font u-font-raleway">
+              Abdallah Salia
+            </h4>
+            <p className="my-0">CO-Founder</p>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-4">
             <img
               src={Image}
               className="img-fluid rounded-circle"
               alt=""
               data-aos="fade-up"
             />
-            <h4 className="mb-0">Joe Dadzie</h4>
-            <p className="my-0">CEO</p>
+            <h4 className="mb-0 u-custom-font u-font-raleway">
+              Michael Osei Twene
+            </h4>
+            <p className="my-0">CO-Founder</p>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-4">
             <img
               src={Image}
               className="img-fluid rounded-circle"
               alt=""
               data-aos="fade-up"
             />
-            <h4 className="mb-0">Joe Dadzie</h4>
-            <p className="my-0">CEO</p>
-          </div>
-          <div className="col-md-3">
-            <img
-              src={Image}
-              className="img-fluid rounded-circle"
-              alt=""
-              data-aos="fade-up"
-            />
-            <h4 className="mb-0">Joe Dadzie</h4>
-            <p className="my-0">CEO</p>
+            <h4 className="mb-0 u-custom-font u-font-raleway">Oppong Kwame</h4>
+            <p className="my-0">Farm Manager</p>
           </div>
         </div>
         <div className="row d-flex justify-content-center pb-5">
           <div className="col-md-3 text-center">
-            <a
-              href="http://webneel.com/30-incredible-and-award-winning-fashion-photography-cergelyte-khokhlov-and-witzel"
-              className="mybutton"
-              title="learn more"
-              target="_blank"
-              rel="noreferrer"
-            >
+            <a href="/about" className="mybutton" rel="noreferrer">
               SEE MORE
             </a>
           </div>
